1ae49b057ff20f76bc0185b0a54f37c4ebe83bd8
Don Huynh <donqhuynh@gmail.com>
2019-11-12 14:44:21 -0800

    Max height 100%.

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/_nuxt/1e6cc080d8ec82d64749.js",
+    "revision": "3a997609078f038cb7a7bda85f5dc61c"
+  },
+  {
     "url": "/_nuxt/20c6d0dd8bc545a6a27e.js",
     "revision": "48c4f19265be36589f65fbcd41288953"
   },
   {
-    "url": "/_nuxt/289dcb639770607c00d3.js",
-    "revision": "7282430098a33398b0865e2f7f72d048"
-  },
-  {
-    "url": "/_nuxt/60bda8568259569a1206.js",
-    "revision": "ffb40fcd10f36b8de4b1e84a79eb9cae"
-  },
-  {
-    "url": "/_nuxt/b999597d4a97b8173206.js",
-    "revision": "5860c3c353eb2ef9aa43a44570600d36"
+    "url": "/_nuxt/55c708638b1c959886e6.js",
+    "revision": "40f25443747d86f04765a1d2658cf568"
   },
   {
     "url": "/_nuxt/e51be0a2cfe3fa7a8972.js",
     "revision": "5506e1c2ef25573e050543180b2d9080"
+  },
+  {
+    "url": "/_nuxt/f59da0de764e8d8fe523.js",
+    "revision": "4c27ca5e151801f17c0bcee5117a690b"
   }
 ], {
   "cacheId": "dnhynh",
