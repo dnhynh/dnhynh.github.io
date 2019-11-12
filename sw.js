dad2e9f726ca31f97519766c7b1985dfcfa4c7a9
Don Huynh <donqhuynh@gmail.com>
2019-11-12 14:12:42 -0800

    Container sizing updates.

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/135342877d97f060cc1f.js",
-    "revision": "2101f5a27f2efaaa98b5675b261011da"
+    "url": "/_nuxt/20c6d0dd8bc545a6a27e.js",
+    "revision": "48c4f19265be36589f65fbcd41288953"
   },
   {
-    "url": "/_nuxt/676c751bf903c6c82acb.js",
-    "revision": "0f72c3709b28f77d82183786cb717d42"
+    "url": "/_nuxt/289dcb639770607c00d3.js",
+    "revision": "7282430098a33398b0865e2f7f72d048"
   },
   {
-    "url": "/_nuxt/799f8662f78bab6d2f4b.js",
-    "revision": "c6fd92aae189d6878714fcc624631360"
+    "url": "/_nuxt/60bda8568259569a1206.js",
+    "revision": "ffb40fcd10f36b8de4b1e84a79eb9cae"
   },
   {
-    "url": "/_nuxt/94027fe1cb9f4ddd2025.js",
-    "revision": "f7aba4854641a116aef0fc3b5aeb4625"
+    "url": "/_nuxt/b999597d4a97b8173206.js",
+    "revision": "5860c3c353eb2ef9aa43a44570600d36"
   },
   {
-    "url": "/_nuxt/f457059035404585b1d3.js",
-    "revision": "8003448d68c5c0532ec9f299339b3195"
+    "url": "/_nuxt/e51be0a2cfe3fa7a8972.js",
+    "revision": "5506e1c2ef25573e050543180b2d9080"
   }
 ], {
   "cacheId": "dnhynh",
